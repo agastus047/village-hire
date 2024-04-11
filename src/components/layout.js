f96441c267d0e5fd381efc18160e5372992fc842
Agastus Joyson <agastusjoyson@gmail.com>
2024-04-12 00:19:55 +0530

    add supabase

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,31 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Head from "next/head";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { useContext } from "react";
+import { RoleContext } from "@/contexts/RoleContext";
+import { useRouter } from "next/router";
 
 export default function Layout({children}) {
+
+    const {status,data:session} = useSession();
+    const {roleState} = useContext(RoleContext);
+    const [role,setRole] = roleState;
+
+    useEffect(() => {
+        (async ()=> {
+            if(status==="authenticated") {
+                if(role==="employee") {
+
+                }
+                else if(role==="employer") {
+
+                }
+            }
+        })();
+    },[status]);
+
     return(<>
         <Head>
             <title>Village Hire</title>
