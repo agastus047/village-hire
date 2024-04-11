6658996d42ba1c0e9d6679b52e8d14d6159f5407
Agastus Joyson <agastusjoyson@gmail.com>
2024-04-11 23:52:53 +0530

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,31 @@
-import Link from 'next/link';
 import React from 'react';
+import { useRouter } from 'next/router';
+import { useContext } from 'react';
+import { RoleContext } from '@/contexts/RoleContext';
 
 const EmployerHomePage = () => {
+
+  const router = useRouter();
+
+  const {roleState} = useContext(RoleContext);
+  const [role,setRole] = roleState;
+
+  const handleEmployeeClick = () => {
+    setRole("employee");
+    router.push('/employee_home');
+  };
+
+  const handleEmployerClick = () => {
+    setRole("employer");
+    router.push('/employer_home');
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="font-bold text-6xl mb-4">Choose a role</h1>
       <div className='flex gap-16 mt-9'>
-        <Link href={"employee_home"}><button className='border border-lightblue rounded-md cursor-pointer p-2 w-36 bg-teal-600 hover:bg-teal-800'>Job Seeker</button></Link>
-        <Link href={"employer_home"}><button className='border border-lightblue rounded-md cursor-pointer p-2 w-36 bg-teal-600 hover:bg-teal-800'>Job Provider</button></Link>
+        <button onClick={handleEmployeeClick} className='border border-lightblue rounded-md cursor-pointer p-2 w-36 bg-teal-600 hover:bg-teal-800'>Job Seeker</button>
+        <button onClick={handleEmployerClick} className='border border-lightblue rounded-md cursor-pointer p-2 w-36 bg-teal-600 hover:bg-teal-800'>Job Provider</button>
       </div>
     </div>
   );
