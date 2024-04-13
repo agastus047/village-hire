0faaabe24ba7fd2740495a15f14e6b1fb4854d43
Agastus Joyson <agastusjoyson@gmail.com>
2024-04-13 17:44:53 +0530

    add employee profile page

--- src/pages/employee_home/index.js
+++ src/pages/employee_home/index.js
@@ -6,22 +6,17 @@ const EmployeeHomePage = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen pt-20" style={{backgroundImage: "url('/blue-ink-wave-flowing-underwater-smoothly-generated-by-ai_188544-41061.jpg')", backgroundSize: "cover", backgroundPosition: "center"}}>
-<div className="flex items-center justify-center">
-<div className="relative">
-  <p className="text-center text-5xl font-bold text-white">Empowering Villages, Empowering Lives</p>
-</div>
+      <div className="flex items-center justify-center">
+      <div className="relative">
+        <p className="text-center text-5xl font-bold text-white">Empowering Villages, Empowering Lives</p>
+      </div>
 
-</div>
+      </div>
 
       <div className='flex gap-8 mt-10'>
         <Link href="joblist">
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 Available Jobs
-            </button>
-        </Link>
-        <Link href={"employee_edit_profile"}>
-            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
-                Edit Profile
             </button>
         </Link>
       </div>
