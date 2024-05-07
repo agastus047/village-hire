0074903d1bdf4421bcc431d308e99d15b47653ab
Agastus Joyson <agastusjoyson@gmail.com>
2024-05-07 22:34:56 +0530

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ const EmployerHomePage = () => {
     <>
     {status==="authenticated" ? (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <img src="/man-organic-farming-concept-illustration_23-2148441820.jpg" alt="Description of the image" class="w-32 h-32 rounded-full mb-4 shadow-lg" />
+      <img src="/man-organic-farming-concept-illustration_23-2148441820.jpg" alt="Description of the image" className="w-32 h-32 rounded-full mb-4 shadow-lg" />
       <h1 className="font-bold text-6xl mb-4">Choose a role</h1>
       <div className='flex gap-16 mt-9'>
         <button onClick={handleEmployeeClick} className='border border-lightblue rounded-md cursor-pointer p-2 w-36 bg-teal-600 hover:bg-teal-800'>Job Seeker</button>
@@ -33,7 +33,7 @@ const EmployerHomePage = () => {
     :
     (
 <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-<img src="/man-organic-farming-concept-illustration_23-2148441820.jpg" alt="Description of the image" class="w-32 h-32 rounded-full mb-4 shadow-lg" />
+<img src="/man-organic-farming-concept-illustration_23-2148441820.jpg" alt="Description of the image" className="w-32 h-32 rounded-full mb-4 shadow-lg" />
   <h1 className="text-4xl font-bold mb-8 text-gray-800">Please Sign In</h1>
   <button
     onClick={() => signIn("google")}
